2fd515fec1369916531f7a20250bb7f06def7ca6
FlorianBGdu75 <florian.segard-gahery@epita.fr>
2024-11-12 00:14:12 +0100

    idk

--- files/src/backgroundestimationfirsttry/labConverter.hpp
+++ files/src/backgroundestimationfirsttry/labConverter.hpp
@@ -7,6 +7,15 @@
     extern "C" {
 #endif
 
+// typedef enum {
+//     CPU,
+//     GPU
+// } e_device_t;
+
+// typedef struct  {
+//     e_device_t device;
+// } Parameters;
+
 void labConv_init(Parameters* params);
 
 void labConv_process_frame(ImageView<rgb8> rgb_image, ImageView<lab> lab_image);
