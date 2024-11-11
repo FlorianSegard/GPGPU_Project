73c2e80ee424ac181fbb8d5401be782463e38384
Alex DR <alexandre.devaux-riviere@epita.fr>
2024-11-11 22:00:51 +0100

    push toward cuda vm

--- files/src/filter_impl.h
+++ files/src/filter_impl.h
@@ -4,10 +4,6 @@
     extern "C" {
 #endif
 
-#include <gst/gst.h>
-#include <gst/video/video.h>
-#include <gst/video/gstvideofilter.h>
-
 void filter_impl(uint8_t* pixels_buffer, int width, int height, int plane_stride, int pixel_stride, GstClockTime timestamp);
 
 #ifdef __cplusplus
