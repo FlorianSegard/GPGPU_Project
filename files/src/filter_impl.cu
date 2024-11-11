9c40679d6d65217c708daf853c2cf4fd3e8ba1d1
Alex DR <alexandre.devaux-riviere@epita.fr>
2024-11-11 22:57:14 +0100

    push toward cuda vm

--- files/src/filter_impl.cu
+++ files/src/filter_impl.cu
@@ -8,8 +8,7 @@
 #define CHECK_CUDA_ERROR(call) do { \
     cudaError_t err = (call); \
     if (err != cudaSuccess) { \
-        printf("CUDA error at %s %d: %s\n", __FILE__, __LINE__, \
-               cudaGetErrorString(err)); \
+        printf("CUDA error at %s %d: %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
         exit(EXIT_FAILURE); \
     } \
 } while(0)
