e948a218325075c31ac9b6468f912f475df670d7
Alex DR <alexandre.devaux-riviere@epita.fr>
2024-11-11 22:19:43 +0100

    push toward cuda vm

--- files/src/filter_impl.h
+++ files/src/filter_impl.h
@@ -4,6 +4,10 @@
 #include <cuda_runtime.h>
 #include <gst/gst.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // RGB color structure
 struct rgb8 {
     uint8_t r;
@@ -46,9 +50,7 @@ __global__ void hysteresis_reconstruction(lab* input, bool* output,
 __global__ void apply_mask(rgb8* input, bool* mask,
                            int width, int height, size_t pitch);
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+
 
 void filter_impl(uint8_t* pixels_buffer, int width, int height,
                  int plane_stride, int pixel_stride, GstClockTime timestamp);
