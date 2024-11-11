2394bd09f62ef7d6cf51dfca0b00f4c3c1f4792c
Alex DR <alexandre.devaux-riviere@epita.fr>
2024-11-11 22:53:42 +0100

    push toward cuda vm

--- files/src/filter_impl.cu
+++ files/src/filter_impl.cu
@@ -5,15 +5,14 @@
 #include <iostream>
 
 // Cuda error checking macro
-#define CHECK_CUDA_ERROR(call) \
-    do { \
-        cudaError_t err = call; \
-        if (err != cudaSuccess) { \
-            printf("CUDA error at %s %d: %s\n", __FILE__, __LINE__, \
-                   cudaGetErrorString(err)); \
-            exit(EXIT_FAILURE); \
-        } \
-    } while (0)
+#define CHECK_CUDA_ERROR(call) do { \
+    cudaError_t err = (call); \
+    if (err != cudaSuccess) { \
+        printf("CUDA error at %s %d: %s\n", __FILE__, __LINE__, \
+               cudaGetErrorString(err)); \
+        exit(EXIT_FAILURE); \
+    } \
+} while(0)
 
 size_t background_ref_pitch;
 lab* background_ref = nullptr;
