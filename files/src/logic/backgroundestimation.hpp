9b9950f3283fa5120153faa4e2b8cafb016565f3
alexandre.devaux-riviere <alexandre.devaux-riviere@epita.fr>
2024-11-12 22:48:01 +0100

    sending to vm for test

--- files/src/logic/backgroundestimation.hpp
+++ files/src/logic/backgroundestimation.hpp
@@ -18,9 +18,9 @@
 
 void background_init(Parameters* params);
 
-void background_process_frame(ImageView<lab> in, ImageView<lab> currentBackground, 
-                ImageView<lab> candidateBackground, ImageView<int> currentTimePixels, 
-                ImageView<float> currentDistancePixels);
+void background_process_frame(Image<lab> in, Image<lab> currentBackground,
+                Image<lab> candidateBackground, Image<int> currentTimePixels,
+                Image<float> currentDistancePixels);
 
 #ifdef __cplusplus
     }
