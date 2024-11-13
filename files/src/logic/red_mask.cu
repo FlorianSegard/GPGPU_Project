68e3162fb0d9651766cb40986e03317abd07a7ef
Alex DR <alexandre.devaux-riviere@epita.fr>
2024-11-14 00:49:55 +0100

    push toward vm for tests

--- files/src/logic/red_mask.cu
+++ files/src/logic/red_mask.cu
@@ -13,9 +13,7 @@ __global__ void red_mask_kernel(ImageView<bool> hysteresis_buffer, ImageView<rgb
     bool hyst_value = (bool*)((std::byte*)hysteresis_buffer.buffer + y * hysteresis_buffer.stride)[x];
     rgb8* rgb_value = (rgb8*)((std::byte*)rgb_buffer.buffer + y * rgb_buffer.stride);
 
-    rgb_value[x].r = rgb_value[x].r / 2 + (hyst_value ? 127 : 0);
-    rgb_value[x].g = rgb_value[x].g / 2;
-    rgb_value[x].b = rgb_value[x].b / 2;
+    rgb_value[x].r = min(rgb_value[x].r + 127 * hyst_value, 255);
 }
 
 void red_mask_cu(ImageView<bool> hysteresis_buffer, ImageView<rgb8> rgb_buffer, int width, int height)
