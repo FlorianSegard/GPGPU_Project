731c1292b89045635d6b30e804055af3e0409c25
Alex DR <alexandre.devaux-riviere@epita.fr>
2024-11-13 20:16:48 +0100

    push toward vm for tests

--- files/src/logic/red_mask.cu
+++ files/src/logic/red_mask.cu
@@ -7,11 +7,17 @@ __global__ void red_mask_kernel(ImageView<bool> hysteresis_buffer, ImageView<rgb
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (x < width && y < height) {
-        if (hysteresis_buffer.buffer[y * stride + x]) {
-                    rgb_buffer.buffer[y * stride + x * 3] = min(255, rgb_buffer.buffer[y * stride + x * 3] + 0.5 * 255);
-        }
-    }
+    if (x >= width || y >= height)
+        return;
+    bool hyst_value = (bool*)((std::byte*)hysteresis_buffer.buffer + y * hysteresis_buffer.stride)[x];
+    rgb8 rgb_value = (rgb8*)((std::byte*)rgb_buffer.buffer + y * rgb_buffer.stride)[x];
+
+    rgb_value.r = rgb_value.r / 2 + (hyst_value[x] ? 127 : 0);
+    rgb_value.g = rgb_value.g / 2;
+    rgb_value.b = rgb_value.b / 2;
+
+    rgb8* lineptr_rgb= (rgb8*)((std::byte*)rgb_buffer.buffer + y * rgb_buffer.stride);
+    lineptr_rgb[x] = rgb_value;
 }
 
 void red_mask_cu(ImageView<bool> hysteresis_buffer, ImageView<rgb8> rgb_buffer, int width, int height, std::ptrdiff_t stride)
