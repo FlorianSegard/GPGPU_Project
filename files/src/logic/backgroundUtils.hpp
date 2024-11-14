3680970175170a2814170b9343bcfa4a7ee2f57f
FlorianBGdu75 <florian.segard-gahery@epita.fr>
2024-11-14 21:30:25 +0100

    changed distance

--- files/src/logic/backgroundUtils.hpp
+++ files/src/logic/backgroundUtils.hpp
@@ -6,14 +6,17 @@
 // Shared to average lab pixels
 __device__ inline lab averageLAB(lab p1, lab p2) {
     lab result;
-    result.L = (p1.L + p2.L) / 2;
-    result.a = (p1.a + p2.a) / 2;
-    result.b = (p1.b + p2.b) / 2;
+    result.L = (p1.L + p2.L) / 2.0f;
+    result.a = (p1.a + p2.a) / 2.0f;
+    result.b = (p1.b + p2.b) / 2.0f;
     return result;
 }
 
 
 // Shared Euclidean distance
 __device__ inline float labDistance(lab p1, lab p2) {
-    return sqrt(pow(p1.L - p2.L, 2) + pow(p1.a - p2.a, 2) + pow(p1.b - p2.b, 2));
+    float dL = p1.L - p2.L;
+    float da = p1.a - p2.a;
+    float db = p1.b - p2.b;
+    return sqrtf(dL * dL + da * da + db * db);
 }
