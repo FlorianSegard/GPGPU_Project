c26f3dec356a1adc59d8a35cffb59cd4a4196655
Alex DR <alexandre.devaux-riviere@epita.fr>
2024-11-16 20:58:00 +0100

    add cpp implem and redicrection to the right engine

--- src/logic/red_mask/red_mask.cpp
+++ src/logic/red_mask/red_mask.cpp
@@ -2,7 +2,16 @@
 
 void red_mask_cpp(ImageView<bool> hysteresis_buffer, ImageView<rgb8> rgb_buffer, int width, int height)
 {
-    return;
+    for (int y = 0; y < height; ++y)
+    {
+        bool hyst_value = (bool*)((std::byte*)hysteresis_buffer.buffer + y * hysteresis_buffer.stride);
+        rgb8* rgb_value = (rgb8*)((std::byte*)rgb_buffer.buffer + y * rgb_buffer.stride);
+
+        for (int x = 0; x < width; ++x)
+        {
+            rgb_value[x].r = min(rgb_value[x].r + 127 * hyst_value[x], 255);
+        }
+    }
 }
 
 extern "C" {
