4af620d09dff91c5bf3af0414425ca397b0755ad
FlorianBGdu75 <florian.segard-gahery@epita.fr>
2024-11-17 16:42:35 +0100

    improvements

--- src/logic/hysteresis/hysteresis.cu
+++ src/logic/hysteresis/hysteresis.cu
@@ -112,7 +112,7 @@ __global__ void hysteresis_kernel(ImageView<bool> upper, ImageView<bool> lower, 
         }
         return;
     }
-    upper_lineptr[x] = false;
+    upper_lineptr[x] = true;
 
 }
 
